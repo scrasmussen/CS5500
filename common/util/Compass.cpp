704011216c3a728cbdd4984158affd3995d7151a
Soren <soren.rasmussen@aggiemail.usu.edu>
2015-04-30 10:04:30 -0600

    Changed to more efficient parallel algorithm

--- common/util/Compass.cpp
+++ common/util/Compass.cpp
@@ -4,75 +4,78 @@
 #include "tbb/blocked_range.h"
 #include "tbb/concurrent_queue.h"
 #include "../world.h"
-
-#include <array>
+#include "chunkmanager.h"
 #include <iostream>
-#include <typeinfo>
 
-Compass::Compass(World input, glm::vec3 pos)
+int modx(int s)
 {
-  world = input;
+  return (s>0?s%ChunkManager::BOUNDX :(s+ChunkManager::BOUNDX)%ChunkManager::BOUNDX);
+};
+
+int mody(int s)
+{
+  return (s>0?s%ChunkManager::BOUNDY :(s+ChunkManager::BOUNDY)%ChunkManager::BOUNDY);
+};
+
+int modz(int s)
+{
+  return s;
+};
+
+Compass::Compass(World inWorld, ChunkManager inChunk, glm::vec3 pos)
+{
+  world = inWorld;
+  chunk = inChunk;
   currentPos = pos;
 };
 
 struct CheckType
 {
-  //const Block* input;
   std::vector<std::shared_ptr<Block> > blocks;
-  glm::vec3 position;
   BlockType type;
-  bool* output;
+  bool* found;
+  ChunkManager* chunk;
+  glm::vec3* distance;
   void operator()( const tbb::blocked_range<int>& range ) const
   {
-        for (int i=range.begin(); i!=range.end(); ++i)
+    for (auto i=range.begin(); i!=range.end(); ++i)
       {
-	    if (blocks[i]->position == position)
-	      {
-		if (blocks[i]->type == type)
-		  output[i] = true;
-		else
-		  output[i] = false;
+	if (type == chunk->get(blocks[i]->position.x, blocks[i]->position.y, blocks[i]->position.z))
+	  {
+	    (*found) = true;
+	    (*distance) = blocks[i]->position;
 	  }
       }
   }
 };
 
 
-int Compass::find(BlockType type)
+glm::vec3 Compass::find(BlockType type)
 {  
   auto blocks = world.blocks;
   auto worldSize = blocks.size();
   auto found = false;
   auto distance = 1;
-  bool boolArray[worldSize];
+  glm::vec3 typeDistance;
 
-  while (!found)
+  while (distance <= static_cast<int>(worldSize)/2)
     {
-      auto q = getQueue(currentPos, distance);
-      auto qSize = q.unsafe_size();
-      glm::vec3 e;      
+      auto q = getQueue(currentPos, distance, worldSize);
+      CheckType check;
+      check.chunk =& chunk;
+      check.blocks = blocks;
+      check.type = type;
+      check.found = &found;
+      check.distance = &typeDistance;
 
-      while (q.try_pop(e))
-	{
-	  CheckType check;
-	  check.blocks = blocks;
-	  check.position = e;
-	  check.type = type;
-	  check.output = boolArray;
+      tbb::parallel_for( tbb::blocked_range<int>(0, q.unsafe_size()), check);
+      if (found)
+	return typeDistance;
 
-	  tbb::parallel_for( tbb::blocked_range<int>( 1, worldSize), check);
-
-	  for (unsigned int i=0; i< worldSize; ++i)
-	    if (boolArray[i] == true)
-	      return distance;
-	}
-
-      if (worldSize == qSize)
-	return -1;
-      distance++;
+       ++distance;
     }
-  
-  return -1;
+
+  return glm::vec3(0,0,0);
 };
 
 struct populatej {
@@ -80,6 +83,7 @@ struct populatej {
   glm::vec3*  start;
   int distance;
   int i;
+  int worldSize;
   void operator()( tbb::blocked_range<int>& range ) const {
     for( int j=range.begin(); j!=range.end(); ++j ){
       int k=i>0?distance-j:distance+j;
@@ -93,6 +97,7 @@ struct populateQueue {
   tbb::concurrent_queue<glm::vec3>* queue;
   glm::vec3* start;
   int distance;
+  int worldSize;
   void operator()( tbb::blocked_range<int>& range ) const {
     for( int i=range.begin(); i!=range.end(); ++i ){
       populatej jloop;
@@ -101,17 +106,18 @@ struct populateQueue {
       jloop.queue=queue;
       jloop.start=start;
       jloop.i=i;
+      jloop.worldSize=worldSize;
       tbb::parallel_for( tbb::blocked_range<int>(-jdistance,jdistance),jloop);
     }
   }
 };
 
-
-tbb::concurrent_queue<glm::vec3> Compass::getQueue( glm::vec3&  start,int dis ) {
+tbb::concurrent_queue<glm::vec3> Compass::getQueue( glm::vec3&  start,int dis , int worldSize) {
   tbb::concurrent_queue<glm::vec3> queue;
   populateQueue pq;
   pq.queue=&queue;
   pq.distance=dis;
+  pq.worldSize=worldSize;
   pq.start=&start;
   tbb::parallel_for( tbb::blocked_range<int>( -dis, +dis ), pq );
   return queue;
